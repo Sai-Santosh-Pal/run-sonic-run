2ff44b1ccc85cb34bfc9ac2cf69e7c4e5ea79126
Sai-Santosh-Pal <saisantoshpal2010@gmail.com>
2025-12-08 20:12:27 +0530

    background effect done

--- src/scenes/mainMenu.js
+++ src/scenes/mainMenu.js
@@ -19,6 +19,10 @@ export default function mainMenu() {
     k.onUpdate(() => {
         if (bgPieces[1].pos.x < 0) {
             bgPieces[0].moveTo(bgPieces[1].pos.x + bgPieceWidth * 2, 0);
+            bgPieces.push(bgPieces.shift());
         }
+
+        bgPieces[0].move(-100, 0)
+        bgPieces[1],moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
     });
 }
